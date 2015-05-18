dcffbddcd8037b159bb28424e1eec1a525501925
Jimmy Oliger <hi@jimmyoliger.com>
2015-05-18 13:03:39 -0400

    remove log

--- src/bootstrap.jsx
+++ src/bootstrap.jsx
@@ -4,8 +4,6 @@ import { setTranslations } from './lib/i18n';
 import Ship from './components/ship';
 
 export default function(element, deployment) {
-  console.log('BOOTSTRAP', element, deployment);
-
   let engine = new Engine(deployment);
 
   setTranslations(deployment.ship.translations);
